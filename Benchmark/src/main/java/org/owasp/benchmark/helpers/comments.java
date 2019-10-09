5704e3065c765dbf584a79fd007583d2be25d0ad
Gawali M <amit.gawali.m@accenture.com>
2019-10-09 15:59:09 +0530

    tesr

--- Benchmark/src/main/java/org/owasp/benchmark/helpers/comments.java
+++ Benchmark/src/main/java/org/owasp/benchmark/helpers/comments.java
@@ -186,6 +186,48 @@ public  class TC01_DockerSmokeTestCase extends Testbase {
 			e.printStackTrace();
 		}
 
+	try{
+Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+c= DriverManager.getConnection( "jdbc:odbc:abc","root","root");
+s=c.createStatement();
+rs= s.executeQuery("select * from exam");
+//s=c.createStatement();
+//s=c.createStatement();
+//s=c.createStatement();
+//s=c.createStatement();
+
+//s=c.createStatement();
+s=c.createStatement();
+//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+//c= DriverManager.getConnection( "jdbc:odbc:abc","root","root");
+//s=c.createStatement();
+//rs= s.executeQuery("select * from exam");
+
+}catch(Exception e) { System.out.println(e); }
+b1= new Button("Submit");
+b2= new Button("Start");
+b1.addActionListener(this);
+b2.addActionListener(this);
+t1= new TextArea(100,100);
+g= new CheckboxGroup();
+c1= new Checkbox("Option1",g, true);
+c2= new Checkbox("Option2",g, false);
+c3= new Checkbox("Option3",g, false);
+add(t1);
+add(c1);
+add(c2);
+add(c3);
+add(b1);
+add(b2);
+
+setLayout( new GridLayout(4,4));
+addWindowListener(this);
+setVisible(true);
+setSize(400,400);
+setLocation(0,0);
+}
+
+
 	}
 	
 	
